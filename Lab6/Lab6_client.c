3c3ca20b2af9c2acf27c696cbd22a517daf051e4
Devon <yhes3103@gmail.com>
2024-11-18 12:39:49 +0800

    add Lab6

--- Lab6/Lab6_client.c
+++ Lab6/Lab6_client.c
@@ -27,7 +27,7 @@ int main(int argc, char* argv[]){
     socklen_t addrlen = sizeof(address);
  
     address.sin_family = AF_INET;
-    address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    address.sin_addr.s_addr = inet_addr(argv[1]);
     address.sin_port = htons(port);
 
     if(connect(server_fd,(struct sockaddr *)&address, addrlen) < 0){
